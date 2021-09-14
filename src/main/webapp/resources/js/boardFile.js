cd7f19b2b20917b7cba137477fb12cd3a718a256
Ji-subin-97 <83721406+Ji-subin-97@users.noreply.github.com>
2021-09-14 10:26:16 +0900

    11

--- src/main/webapp/resources/js/boardFile.js
+++ src/main/webapp/resources/js/boardFile.js
@@ -4,14 +4,19 @@
  	최대 5개까지 만 추가 가능
   
  */
- 	let file = '<div class="input-group mb-3">';
-  	file = file + '<input type="file" class="form-control">';
-  	file = file + '<button class="btn btn-outline-secondary del" type="button"><span>X</span></button>';
-	file = file + '</div>';
+ 	
  	
  	let count = 0;
+ 	let index = 0;
  	
  	$("#add_file").click(function(){
+		
+		index++;
+		
+		let file = '<div id="del'+ index +'"class="input-group mb-3">';
+	  	file = file + '<input type="file" class="form-control">';
+	  	file = file + '<button data-btn-id="'+ index +'" class="btn btn-outline-secondary del" type="button">X</button>';
+		file = file + '</div>';
 		
 		if(count<5){
 			count++;
@@ -30,7 +35,11 @@
 	});*/
 	
 	$("#fileAddResult").on('click', '.del', function(){
-		console.log(this);
+		
+		let num = $(this).attr("data-btn-id");;
+		count--;
+		$("#del"+ num).remove();
+		
 	});
 
 	
